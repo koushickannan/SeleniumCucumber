898bbd671dc2409d9f3618c385c92e30edb6214f
Koushic Kannan <koushic-kannan.raju@hpe.com>
2017-03-09 12:39:12 +0530

    Update 09/03/2017 12.39PM

--- automation/src/test/java/cucumberTest/AddAsset.java
+++ automation/src/test/java/cucumberTest/AddAsset.java
@@ -1,5 +1,75 @@
 package cucumberTest;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.After;
+//import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class AddAsset {
+
+	WebDriver driver;
+
+	Config co = new Config();
+	Prep prep = new Prep();
+	Select s;
+	Platform po = new Platform();
+
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+
+		System.setProperty("webdriver.chrome.driver", "./CHDriver/chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// po.readBrowserName();
+		//
+		// driver.get(po.readURL());
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(co.getBaseUrl());
+	}
+
+	@Test
+	public void loginForm() throws Exception {
+		try {
+
+			// String baseUrl = "http://15.213.51.128:8080/dsm/";
+
+			driver.get(co.getBaseUrl());
+			driver.findElement(By.id("username")).sendKeys(co.getUserName());
+			driver.findElement(By.id("password")).sendKeys(co.getPassword());
+			driver.findElement(By.xpath("//input[@value='Log in']")).click();
+
+		} catch (Exception e) {
+			verificationErrors.append(e.getMessage());
+			throw e;
+		}
+
+	}
+
+	@Test
+	public void addUser() throws Exception {
+		driver.findElement(By.xpath("//*[contains(@class,'nav-label') and text()='Asset Mgmt.']")).click();
+		driver.findElement(By.xpath("//a/li[contains(text(),'Add Asset') and @id='asset_2']")).click();
+		
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+
+	}
 
 }
